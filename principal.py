10ca4f51b363c26ee19ac43a0b536a473af1a75d
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-01-08 17:59:32 -0300

    Acicionando parte 3/5 do curso

--- principal.py
+++ principal.py
@@ -1,4 +1,10 @@
-arquivo_contatos = open("dados/contatos.csv", encoding="latin_1")
-
-for linha in arquivo_contatos:
-    print(linha, end='')
+try:
+    with open("dados/contatos.csv", encoding="latin_1") as arquivo_contatos:
+        for linha in arquivo_contatos:
+            print(linha, end='')
+except FileNotFoundError:
+    print("Arquivo não encontrado")
+except PermissionError:
+    print("Sem permissao de escrita")
+finally:
+    arquivo_contatos.close()
